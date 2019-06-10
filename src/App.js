ae1d0525639379b589cf9623b88aaf094196a4e6
ramu <ramu@gmail.com>
2019-06-10 07:43:50 +0000

    reactapp modifies

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
+{/*      <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
@@ -18,7 +18,10 @@ function App() {
         >
           Learn React
         </a>
-      </header>
+      </header> */}
+<h1>hello world</h1>
+<p>A wiki is a knowledge base website on which users collaboratively modify content and structure directly from the web browser. In a typical wiki, text is written ..</p>
+
     </div>
   );
 }
